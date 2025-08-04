addb9d69f08872e412e21bf5661bada975e83dc0
Brian-Rada <Brian.Rada@ibm.com>
2025-08-04 12:00:59 +0800

    fix: increase bin lookup

--- src/rad.c
+++ src/rad.c
@@ -86,7 +86,7 @@ int is_readable( FILE *file ) {
     while ( (chunk = fgetc( file )) != EOF && ctr < 1024 ) {
         if( !(isspace( chunk ) || isprint( chunk ) ) ) {
             bin++;
-            if ( bin > 8 ) {
+            if ( bin > 20 ) {
                 rewind( file );
                 return 0;
             }
